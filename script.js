8ea8c6c7c4b415f7bef7d4375341dfea58b858ea
corbin Boughen <50217007+theBough@users.noreply.github.com>
2023-05-02 14:58:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var whoseTurn = "O"
 function makeGrid() {
   for (i = 0; i < 9; i++) {
     var btn = document.createElement("button");
@@ -6,5 +7,16 @@ function makeGrid() {
     btn.setAttribute("id", i);
     btn.setAttribute("innerHTML", "");
     document.getElementById("grid-container").appendChild(btn)
+    document.getElementById(i).onclick = thisClick
   }//end For loop
 }//end function
+
+var thisClick = function(){
+  var thisBtn = document.getElementById(this.id);
+  //Check to see that the square is available.
+  if(thisBtn.innerHTML != ""){
+    alert("That square is taken!!");
+    return 0;
+  }
+  thisBtn.innerHTML = whoseTurn;
+}
