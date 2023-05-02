433a5f2a0462d26aed4d8675b9237407412f9014
corbin Boughen <50217007+theBough@users.noreply.github.com>
2023-05-02 14:39:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-function makeGrid(){
-  var btn = document.createElement("button");
-  btn.setAttribute("type", "button");
-  btn.setAttribute("class", "btnClass");
-  btn.setAttribute("id","first");
-  btn.setAttribute("innerHTML", "");
-
-  document.getElementById("grid-container").appendChild(btn)
-}
+function makeGrid() {
+  for (i = 0; i < 9; i++) {
+    var btn = document.createElement("button");
+    btn.setAttribute("type", "button");
+    btn.setAttribute("class", "btnClass");
+    btn.setAttribute("id", i);
+    btn.setAttribute("innerHTML", "");
+    document.getElementById("grid-container").appendChild(btn)
+  }//end For loop
+}//end function
